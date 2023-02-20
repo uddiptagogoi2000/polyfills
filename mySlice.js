a567ec92ef3d81c9413e94de438bf2af142c73ba
uddiptagogoi2000 <uddiptagogoi2000@gmail.com>
2023-02-20 14:31:51 +0530

    feat: add slice method

---
+++ mySlice.js
@@ -0,0 +1,26 @@
+Array.prototype.mySlice = function (start = 0, end = this.length) {
+    const slice = [];
+    // -ve cases
+    // case 1: "one arg passed / start is passed"
+    start = start < 0 ? end + start : start; // hash start value -> converting -ve to corresponding +ve;
+    end = end < 0 ? end + end : end;
+    // start = !start || 0; // if start is out of bound i.e; start is less than 0
+    if (start > end) {
+        start = 0;
+        end = 0;
+    }
+    if (start < 0) {
+        // after hasing if start is still -ve
+        start = !start || 0; // if start is out of bound i.e; start is less than 0
+    }
+
+    // +ve cases
+    // case 1: "no arg passed"
+    // case 2: "arg start passed / only one arg"
+    // case 3: "both the start and end were passed"
+    for (let i = start; i < end; i++) {
+        slice.push(this[i]);
+    }
+
+    return slice;
+};
