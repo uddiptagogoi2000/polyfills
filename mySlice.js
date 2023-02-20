1c5a0b07645d370bda4848a5e218cef353718a4e
uddiptagogoi2000 <uddiptagogoi2000@gmail.com>
2023-02-20 15:20:19 +0530

    refactor: convert if statement to ternary

--- mySlice.js
+++ mySlice.js
@@ -1,7 +1,6 @@
 Array.prototype.mySlice = function (start = 0, end = this.length) {
     const slice = [];
     // -ve cases
-    // case 1: "one arg passed / start is passed"
     start = start < 0 ? this.length + start : start; // hash start value -> converting -ve to corresponding +ve;
     end = end < 0 ? this.length + end : end;
     // start = !start || 0; // if start is out of bound i.e; start is less than 0
@@ -9,10 +8,8 @@ Array.prototype.mySlice = function (start = 0, end = this.length) {
         start = 0;
         end = 0;
     }
-    if (start < 0) {
-        // after hasing if start is still -ve
-        start = !start || 0; // if start is out of bound i.e; start is less than 0
-    }
+    // after hasing if start is still -ve
+    start = start < 0 ? 0 : start;
 
     // +ve cases
     // case 1: "no arg passed"
